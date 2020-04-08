ec29efa2b63644a8971c92d2a6b7c9b5245517ba
newDan1 <43250455+newDan1@users.noreply.github.com>
2020-04-08 13:16:24 +0300

    Add versions 1.1.0 to 1.4.0

---
+++ dimin 1.0.0/shell.py
@@ -0,0 +1,20 @@
+
+import dimin
+import colorama
+
+colorama.init(convert=True)
+
+print(colorama.Fore.GREEN + 'Shell' + colorama.Style.RESET_ALL)
+
+while True:
+    try:
+        print(colorama.Fore.GREEN + "dimin > " + colorama.Fore.LIGHTMAGENTA_EX, end="")
+        text = input('')
+        print(colorama.Style.RESET_ALL, end="")
+    except BaseException:
+        text = ""
+    result, error = dimin.run('<shell>', text)
+
+    # if error: print(error.as_string())
+    if error: print(colorama.Fore.RED + error.as_string() + colorama.Style.RESET_ALL)
+    else: print(colorama.Fore.BLUE + str(result) + colorama.Style.RESET_ALL)
